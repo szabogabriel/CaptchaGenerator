bc301b28db561ada949a58e2c34292357a682c8c
szabogabriel <szabo.gabriel@gmail.com>
2022-01-23 13:38:28 +0100

    Change test

--- Captcha Generator/test/captcha/CaptchaTest.java
+++ Captcha Generator/test/captcha/CaptchaTest.java
@@ -1,20 +1,42 @@
 package captcha;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import captcha.Captcha;
 import captcha.hash.DefaultHashCalculator;
 import captcha.printer.DefaultImagePrinter;
 
 public class CaptchaTest {
 	
+	File targetFile = new File("test.png");
+	
+	@BeforeEach
+	void initClean() {
+		if (targetFile.exists()) {
+			targetFile.delete();
+		}
+	}
+	
+	@AfterEach
+	void afterClean() {
+		if (targetFile.exists()) {
+			targetFile.delete();
+		}
+	}
+	
 	@Test
-	void testDefaultCaptcha() throws FileNotFoundException {
-		Captcha c = new Captcha("aBcd342", "jpg", new DefaultImagePrinter(true), new DefaultHashCalculator());
-		c.writeImage(new FileOutputStream("test.jpg"));
+	void testDefaultCaptcha() throws IOException {
+		String input = "aBcd342";
+		Captcha c = new Captcha(input, "png", new DefaultImagePrinter(true), new DefaultHashCalculator());
+		c.writeImage(new FileOutputStream(targetFile));
+		assertTrue(c.isMatchingString(input));
 	}
 	
 }
