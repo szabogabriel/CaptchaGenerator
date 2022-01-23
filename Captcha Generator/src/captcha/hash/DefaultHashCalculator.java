3c5de785b53b56b8ec2561f4ce8e518db98d2ea1
szabogabriel <szabo.gabriel@gmail.com>
2022-01-23 14:00:16 +0100

    Make DefaultHashCalculator serializable

--- Captcha Generator/src/captcha/hash/DefaultHashCalculator.java
+++ Captcha Generator/src/captcha/hash/DefaultHashCalculator.java
@@ -2,6 +2,8 @@ package captcha.hash;
 
 public class DefaultHashCalculator implements HashCalculator {
 
+	private static final long serialVersionUID = 4940445509821742839L;
+
 	@Override
 	public String calculateHash(String value) {
 		return value.hashCode() + "";
